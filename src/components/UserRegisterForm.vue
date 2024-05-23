273b3ee267d66aa88b463d32f7338175801ba6ff
Belen <btejeramaicas@alumnos.viu.es>
2024-05-23 22:29:35 +0200

    CRUD propietario completado

--- src/components/UserRegisterForm.vue
+++ src/components/UserRegisterForm.vue
@@ -7,35 +7,62 @@
                         <v-card class="elevation-12 rounded-lg transparent-card">
                             <v-card-text class="pa-5 mb-5">
                                 <h2 class="text-center mb-10 black--text font-weight-light">REGISTRAR NUEVO USUARIO</h2>
-                                <div class="d-flex align-center mb-5">
-                                    <font-awesome-icon :icon="['fas', 'user']" class="black--text mr-3" />
-                                    <v-text-field v-model="user.username" label="Nombre de Usuario" outlined
-                                        class="black--text flex-grow-1" :rules="[rules.required]">
-                                    </v-text-field>
-                                </div>
-                                <div class="d-flex align-center mt-5 mb-5">
-                                    <font-awesome-icon :icon="['fas', 'lock']" class="black--text mr-3" />
-                                    <v-text-field v-model="user.password" label="Contraseña" type="password" outlined
-                                        class="black--text flex-grow-1" :rules="[rules.required]">
-                                    </v-text-field>
-                                </div>
-                                <div class="d-flex align-center mt-5 mb-5">
-                                    <font-awesome-icon :icon="['fas', 'user-tag']" class="black--text mr-3" />
-                                    <v-select v-model="user.role" :items="roles" label="Selecciona Rol" outlined
-                                        class="black--text flex-grow-1" :rules="[rules.required]">
-                                    </v-select>
-                                </div>
-                                <v-row justify="center">
-                                    <v-col cols="8" md="4">
-                                        <v-btn :color="buttonColor" rounded large block @click="registerUser"
-                                            @mouseover="changeColor" @mouseleave="revertColor" class="mb-2">REGISTRAR</v-btn>
-                                    </v-col>
-                                    <v-col cols="8" md="4">
-                                        <v-btn color="secondary" rounded large block @click="goBack" class="mb-2">Volver</v-btn>
-                                    </v-col>
-                                </v-row>
-                                <v-alert v-if="error" type="error" dense>{{ error }}</v-alert>
-                                <v-alert v-if="success" type="success" dense>{{ success }}</v-alert>
+                                
+                                <v-form ref="form">
+                                    <div class="d-flex align-center mt-5 mb-5">
+                                        <font-awesome-icon :icon="['fas', 'user']" class="black--text mr-3" />
+                                        <v-text-field v-model="user.name" label="Nombre" outlined
+                                            class="black--text flex-grow-1" :rules="[rules.required]">
+                                        </v-text-field>
+                                    </div>
+                                    <div class="d-flex align-center mt-5 mb-5">
+                                        <font-awesome-icon :icon="['fas', 'user']" class="black--text mr-3" />
+                                        <v-text-field v-model="user.surnames" label="Apellidos" outlined
+                                            class="black--text flex-grow-1" :rules="[rules.required]">
+                                        </v-text-field>
+                                    </div>
+                                    <div class="d-flex align-center mt-5 mb-5">
+                                        <font-awesome-icon :icon="['fas', 'phone']" class="black--text mr-3" />
+                                        <v-text-field v-model="user.telephone" label="Teléfono" outlined
+                                            class="black--text flex-grow-1" :rules="[rules.required]">
+                                        </v-text-field>
+                                    </div>
+                                    <div class="d-flex align-center mt-5 mb-5">
+                                        <font-awesome-icon :icon="['fas', 'envelope']" class="black--text mr-3" />
+                                        <v-text-field v-model="user.email" label="E-mail" outlined
+                                            class="black--text flex-grow-1" :rules="[rules.required]">
+                                        </v-text-field>
+                                    </div>
+                                    <div class="d-flex align-center mb-5">
+                                        <font-awesome-icon :icon="['fas', 'user']" class="black--text mr-3" />
+                                        <v-text-field v-model="user.username" label="Nombre de Usuario" outlined
+                                            class="black--text flex-grow-1" :rules="[rules.required]">
+                                        </v-text-field>
+                                    </div>
+                                    <div class="d-flex align-center mt-5 mb-5">
+                                        <font-awesome-icon :icon="['fas', 'lock']" class="black--text mr-3" />
+                                        <v-text-field v-model="user.password" label="Contraseña" type="password" outlined
+                                            class="black--text flex-grow-1" :rules="[rules.required]">
+                                        </v-text-field>
+                                    </div>
+                                    <div class="d-flex align-center mt-5 mb-5">
+                                        <font-awesome-icon :icon="['fas', 'user-tag']" class="black--text mr-3" />
+                                        <v-select v-model="user.role" :items="roles" label="Selecciona Rol" outlined
+                                            class="black--text flex-grow-1" :rules="[rules.required]">
+                                        </v-select>
+                                    </div>
+                                    <v-row justify="center">
+                                        <v-col cols="8" md="4">
+                                            <v-btn :color="buttonColor" rounded large block @click="registerUser"
+                                                @mouseover="changeColor" @mouseleave="revertColor" class="mb-2">REGISTRAR</v-btn>
+                                        </v-col>
+                                        <v-col cols="8" md="4">
+                                            <v-btn color="secondary" rounded large block @click="goBack" class="mb-2">Volver</v-btn>
+                                        </v-col>
+                                    </v-row>
+                                    <v-alert v-if="error" type="error" dense>{{ error }}</v-alert>
+                                    <v-alert v-if="success" type="success" dense>{{ success }}</v-alert>
+                                </v-form>
                             </v-card-text>
                         </v-card>
                     </v-col>
@@ -58,7 +85,11 @@ export default {
         return {
             buttonColor: 'red darken-3',
             user: {
+                name: '',
+                surnames: '',
+                telephone: '',
                 username: '',
+                email: '',
                 password: '',
                 role: ''
             },
@@ -76,26 +107,36 @@ export default {
             this.success = null;
             if (this.$refs.form.validate()) {
                 const urbanization = JSON.parse(localStorage.getItem('urbanizacion'));
-                if (!urbanization || !urbanization.id) {
+                if (!urbanization || !urbanization.id_urbanizacion) {
                     this.error = 'No se encontró la urbanización en localStorage';
                     console.error('No se encontró la urbanización en localStorage');
                     return;
                 }
 
                 const requestData = {
-                    urbanization_id: urbanization.id,
-                    contrasena: this.user.password,
+                    urbanization_id: urbanization.id_urbanizacion,
+                    nombre: this.user.name,
+                    apellidos: this.user.surnames,
+                    telefono: this.user.telephone,
+                    email: this.user.email,
                     username: this.user.username,
-                    rol: this.user.role
+                    contrasena: this.user.password,
+                    rol: this.user.role,
                 };
 
                 try {
                     const response = await api.post('/register', requestData);
                     if (response.data.success) {
                         this.success = 'Usuario registrado exitosamente';
-                        this.user.username = '';
-                        this.user.password = '';
-                        this.user.role = '';
+                        this.user = {
+                            name: '',
+                            surnames: '',
+                            telephone: '',
+                            email: '',
+                            username: '',
+                            password: '',
+                            role: ''
+                        };
                     } else {
                         this.error = response.data.error || 'Error al registrar usuario';
                     }
